beb7f16f79938170842333715d77363c6203e292
Katie Bee <knees4bees@gmail.com>
2021-06-25 14:59:37 -0400

    Add more seed messages

--- db/seeds/dev/03_messages.js
+++ db/seeds/dev/03_messages.js
@@ -17,6 +17,78 @@ exports.seed = async (knex) => {
       sender_id: 1,
       recipient_id: 2,
       content: 'Not much. You?'
+    },
+    {
+      id: 4,
+      sender_id: 2,
+      recipient_id: 1,
+      content: 'Oh not much'
+    },
+    {
+      id: 5,
+      sender_id: 1,
+      recipient_id: 2,
+      content: 'This is an exciting conversation'
+    },
+    {
+      id: 6,
+      sender_id: 2,
+      recipient_id: 1,
+      content: 'Yeah for sure'
+    },
+    {
+      id: 7,
+      sender_id: 2,
+      recipient_id: 1,
+      content: 'Hey I saw something cool the other day'
+    },
+    {
+      id: 8,
+      sender_id: 2,
+      recipient_id: 3,
+      content: 'Hey'
+    },
+    {
+      id: 9,
+      sender_id: 3,
+      recipient_id: 2,
+      content: 'hey how\'s it going'
+    },
+    {
+      id: 10,
+      sender_id: 2,
+      recipient_id: 3,
+      content: 'Good! Guess what'
+    },
+    {
+      id: 11,
+      sender_id: 3,
+      recipient_id: 2,
+      content: 'what'
+    },
+    {
+      id: 12,
+      sender_id: 2,
+      recipient_id: 3,
+      content: 'I have a pet squirrel now'
+    },
+    {
+      id: 13,
+      sender_id: 3,
+      recipient_id: 2,
+      content: 'no way'
+    },
+    {
+      id: 14,
+      sender_id: 2,
+      recipient_id: 3,
+      content: 'No for real'
+    },
+    {
+      id: 15,
+      sender_id: 2,
+      recipient_id: 3,
+      content: 'It\'s really cute. Fluffy tail and everything'
     }])
   } catch (error) {
     console.log(`Error seeding messages: ${error}`);
