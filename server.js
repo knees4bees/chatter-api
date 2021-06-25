6d402387d28fffa41ef8819c95fc1653e12cec2a
Katie Bee <knees4bees@gmail.com>
2021-06-25 17:06:43 -0400

    Set up basic express server

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Chatter API';
+
+app.get('/', (request, response) => {
+  response.send('Welcome to the Chatter API!');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+});
