6fcb2d854c1fc2350d062a6dd0bda4f1b305d5e7
Katie Bee <knees4bees@gmail.com>
2021-06-26 14:48:25 -0400

    Factor fake message data out into separate file

--- db/seeds/dev/03_messages.js
+++ db/seeds/dev/03_messages.js
@@ -1,101 +1,14 @@
+const messagesData = require('../../../messagesData');
+
 exports.seed = async (knex) => {
   try {
-    await knex('messages').insert([{
-      id: 1,
-      sender_id: 1,
-      recipient_id: 2,
-      content: 'Hey hey!',
-      created_at: "2021-03-11T02:26:00.219Z"
-    },
-    {
-      id: 2,
-      sender_id: 2,
-      recipient_id: 1,
-      content: 'Hey what\'s up?',
-      created_at: "2021-03-11T02:26:00.219Z"
-    },
-    {
-      id: 3,
-      sender_id: 1,
-      recipient_id: 2,
-      content: 'Not much. You?'
-    },
-    {
-      id: 4,
-      sender_id: 2,
-      recipient_id: 1,
-      content: 'Oh not much'
-    },
-    {
-      id: 5,
-      sender_id: 1,
-      recipient_id: 2,
-      content: 'This is an exciting conversation'
-    },
-    {
-      id: 6,
-      sender_id: 2,
-      recipient_id: 1,
-      content: 'Yeah for sure'
-    },
-    {
-      id: 7,
-      sender_id: 2,
-      recipient_id: 1,
-      content: 'Hey I saw something cool the other day'
-    },
-    {
-      id: 8,
-      sender_id: 2,
-      recipient_id: 3,
-      content: 'Hey',
-      created_at: "2021-04-18T02:26:00.219Z"
-    },
-    {
-      id: 9,
-      sender_id: 3,
-      recipient_id: 2,
-      content: 'hey how\'s it going',
-      created_at: "2021-04-19T02:26:00.219Z"
-    },
-    {
-      id: 10,
-      sender_id: 2,
-      recipient_id: 3,
-      content: 'Good! Guess what'
-    },
-    {
-      id: 11,
-      sender_id: 3,
-      recipient_id: 2,
-      content: 'what'
-    },
-    {
-      id: 12,
-      sender_id: 2,
-      recipient_id: 3,
-      content: 'I have a pet squirrel now'
-    },
-    {
-      id: 13,
-      sender_id: 3,
-      recipient_id: 2,
-      content: 'no way'
-    },
-    {
-      id: 14,
-      sender_id: 2,
-      recipient_id: 3,
-      content: 'No for real'
-    },
-    {
-      id: 15,
-      sender_id: 2,
-      recipient_id: 3,
-      content: 'It\'s really cute. Fluffy tail and everything'
-    }]);
+    const messagePromises = messagesData.map(message => {
+      return knex('messages').insert(message);
+    });
 
     await knex.raw('SELECT setval(\'messages_id_seq\', MAX(id)) from messages');
+
+    return Promise.all(messagePromises);
   } catch (error) {
     console.log(`Error seeding messages: ${error}`);
   }
---
+++ messagesData.js
@@ -0,0 +1,96 @@
+const messagesData = [{
+  id: 1,
+  sender_id: 1,
+  recipient_id: 2,
+  content: 'Hey hey!',
+  created_at: "2021-03-11T02:26:00.219Z"
+},
+{
+  id: 2,
+  sender_id: 2,
+  recipient_id: 1,
+  content: 'Hey what\'s up?',
+  created_at: "2021-03-11T02:26:00.219Z"
+},
+{
+  id: 3,
+  sender_id: 1,
+  recipient_id: 2,
+  content: 'Not much. You?'
+},
+{
+  id: 4,
+  sender_id: 2,
+  recipient_id: 1,
+  content: 'Oh not much'
+},
+{
+  id: 5,
+  sender_id: 1,
+  recipient_id: 2,
+  content: 'This is an exciting conversation'
+},
+{
+  id: 6,
+  sender_id: 2,
+  recipient_id: 1,
+  content: 'Yeah for sure'
+},
+{
+  id: 7,
+  sender_id: 2,
+  recipient_id: 1,
+  content: 'Hey I saw something cool the other day'
+},
+{
+  id: 8,
+  sender_id: 2,
+  recipient_id: 3,
+  content: 'Hey',
+  created_at: "2021-04-18T02:26:00.219Z"
+},
+{
+  id: 9,
+  sender_id: 3,
+  recipient_id: 2,
+  content: 'hey how\'s it going',
+  created_at: "2021-04-19T02:26:00.219Z"
+},
+{
+  id: 10,
+  sender_id: 2,
+  recipient_id: 3,
+  content: 'Good! Guess what'
+},
+{
+  id: 11,
+  sender_id: 3,
+  recipient_id: 2,
+  content: 'what'
+},
+{
+  id: 12,
+  sender_id: 2,
+  recipient_id: 3,
+  content: 'I have a pet squirrel now'
+},
+{
+  id: 13,
+  sender_id: 3,
+  recipient_id: 2,
+  content: 'no way'
+},
+{
+  id: 14,
+  sender_id: 2,
+  recipient_id: 3,
+  content: 'No for real'
+},
+{
+  id: 15,
+  sender_id: 2,
+  recipient_id: 3,
+  content: 'It\'s really cute. Fluffy tail and everything'
+}];
+
+module.exports = messagesData;
