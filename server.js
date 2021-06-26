84bddea819dc045016e302afb080622ae0ab7d20
Katie Bee <knees4bees@gmail.com>
2021-06-25 20:22:18 -0400

    Connect server to database

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.use(cors());
 app.use(express.json());
@@ -8,10 +11,10 @@ app.use(express.json());
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Chatter API';
 
-app.get('/', (request, response) => {
-  response.send('Welcome to the Chatter API!');
-});
-
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+});
+
+app.get('/', (request, response) => {
+  response.send('Welcome to the Chatter API!');
 });
