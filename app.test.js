d3f1109cd9a87c6e5bd4069c36570a9e216a896a
Katie Bee <knees4bees@gmail.com>
2021-06-27 12:41:35 -0400

    Add basic server test and tweak first GET test

--- app.test.js
+++ app.test.js
@@ -9,23 +9,31 @@ const now = database.fn.now();
 const messageCutoff = '30 day';
 const messageLimit = 100;
 
-describe('GET /api/v1/messages?recipient=3', () => {
+describe('Server', () => {
+  it('should return a 200 status', async () => {
+    const response = await request(app).get('/');
+    expect (response.status).toBe(200);
+  });
+});
+
+// Get recent messages from all senders
+describe('GET /api/v1/messages?recipient=:recipient_id', () => {
 	it('should return a 200 and recent messages from all senders', async () => {
+    const recipient_id = 3;
 
 	  const dbMessages = await database('messages')
-      .where('recipient_id', 3)
+      .where('recipient_id', recipient_id)
       .whereBetween('created_at', [database.raw(`? - ?::INTERVAL`, [now, messageCutoff]), now])
       .orderBy('created_at', 'desc')
       .limit(messageLimit)
       .select();
-    
     const expectedMessages = JSON.parse(JSON.stringify(dbMessages));
 
-	  const response = await request(app).get('/api/v1/messages?recipient=3');
+	  const response = await request(app).get(`/api/v1/messages?recipient=${recipient_id}`);
 	  const messages = response.body;
       
 	  expect(response.status).toBe(200);
-	  expect(messages).toEqual(expectedMessages);
     expect(messages).toHaveLength(50);
+	  expect(messages).toEqual(expectedMessages);
 	});
 });
